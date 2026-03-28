7de0c9502c99cd9219353fd29504b2121cca35e7
Barum31 <berenbatuhandemirkol@gmail.com>
2026-03-28 18:41:01 +0300

    tick speed eklendi her bir bina static bodysine tick speed de bir func çağırma özelliği verildi

--- main.gd
+++ main.gd
@@ -1,5 +1,6 @@
 extends Node
-
+var current_tick = 0
+@export var tick_interval = 5.0
 @onready var menu_camera = $CanvasLayer/MainMenu/MenuCamera
 @onready var player_camera = $CameraPivot/CameraGimbal/PlayerCamera
 @onready var animation_player = $CanvasLayer/MainMenu/AnimationPlayer
@@ -15,5 +16,29 @@ func oyunu_baslat():
 	# Bayrağı oyuncu kamerasına devret
 	player_camera.make_current()
 	player_camera.set_process(true)
+	# Bir Timer oluştur ve bağla
+	var timer = Timer.new()
+	add_child(timer)
+	timer.wait_time = tick_interval
+	timer.autostart = true
+	timer.timeout.connect(_on_tick_timeout)
+	timer.start()
 	
 	# Oyuncunun hareket script'ini burada aktif edebilirsin
+func _on_tick_timeout():
+	current_tick += 1
+	print("Yeni Tick: ", current_tick)
+	apply_tick_mechanics()
+	tick_changed.emit(current_tick)
+func apply_tick_mechanics():
+	# Belirli tick sayılarında farklı olaylar tetikle
+		match current_tick % 4:
+			0:print("event")
+		
+			1:print("event")
+		
+			2:print("event")
+		
+			3:print("event")
+
+signal tick_changed(new_tick_value)
